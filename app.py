e6debd40e0e6267e74cf7129f48bcf5bfe2eac8c
VitaliiFedin <vitalii.fedinn@gmail.com>
2022-07-12 13:38:37 +0300

    model databe create

---
+++ app.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+# serializer
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: int
+    on_offer: bool
+
+
+@app.get('/')
+def index():
+    return {"message": "Hello world"}
+
+
+@app.get('/greet/{name}')
+def greet_name(name: str):
+    return {"Greeting": f"Hello {name}"}
+
+
+@app.put('/item/{id}')
+def update_item(item_id: int, item: Item):
+    return {'name': item.name,
+            'description': item.description,
+            'price': item.price,
+            'on_offer': item.on_offer
+            }
