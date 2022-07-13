f54f45e443dc5be033029220f9d3948d6f611985
VitaliiFedin <vitalii.fedinn@gmail.com>
2022-07-13 14:58:46 +0300

    get, post requests

--- app.py
+++ app.py
@@ -1,32 +1,62 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
+from database import SessionLocal
+from typing import Optional, List
+from models import Item
 
 app = FastAPI()
 
 
 # serializer
-class Item(BaseModel):
+class ItemSerializer(BaseModel):
     id: int
     name: str
     description: str
     price: int
     on_offer: bool
 
-
-@app.get('/')
-def index():
-    return {"message": "Hello world"}
+    class Config:
+        orm_mode = True
 
 
-@app.get('/greet/{name}')
-def greet_name(name: str):
-    return {"Greeting": f"Hello {name}"}
+db = SessionLocal()
 
 
-@app.put('/item/{id}')
-def update_item(item_id: int, item: Item):
-    return {'name': item.name,
-            'description': item.description,
-            'price': item.price,
-            'on_offer': item.on_offer
-            }
+@app.get('/items', response_model=List[ItemSerializer], status_code=200)
+def get_all():
+    items = db.query(Item).all()
+    return items
+
+
+@app.get('/item/{item_id}')
+def get_item(item_id: int):
+    pass
+
+
+@app.post('/item-create/', response_model=ItemSerializer, status_code=status.HTTP_201_CREATED)
+def create_item(item: ItemSerializer):
+    new_item = Item(
+        name=item.name,
+        description=item.description,
+        price=item.price,
+        on_offer=item.on_offer
+
+    )
+    db_item = db.query(Item).filter(item.name == new_item.name).first()
+
+    if db_item is not None:
+        raise HTTPException(status_code=400, detail='Item already exists')
+    db.add(new_item)
+    db.commit()
+
+    return new_item
+
+
+@app.delete('item-delete/{item_id}')
+def delete_item(item_id: int):
+    pass
+
+
+@app.put('item-update/{item_id}')
+def update_item(item_id):
+    pass
